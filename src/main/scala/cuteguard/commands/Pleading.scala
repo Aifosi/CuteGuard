213e8cab59d34bfd81728654865def9cdcb07217
Aifosi <sissygasmquest@gmail.com>
2024-06-30 18:10:29 +0100

    Catch ascii emoji and emoji with diacritics or capitals

--- src/main/scala/cuteguard/commands/Pleading.scala
+++ src/main/scala/cuteguard/commands/Pleading.scala
@@ -4,11 +4,14 @@ import cuteguard.model.Embed
 import cuteguard.model.event.MessageEvent
 
 import cats.effect.IO
+import org.apache.commons.lang3.StringUtils.stripAccents
 import org.typelevel.log4cats.Logger
 
 import scala.util.matching.Regex
 object Pleading extends TextCommand with NoLog:
-  override def pattern: Regex = "<a?:\\w*pleading\\w*:\\d+>".r
+  override def pattern: Regex = ".*(<a?:\\w*pleading\\w*:\\d+>|\uD83E\uDD7A).*".r
+
+  override def matches(event: MessageEvent): Boolean = pattern.matches(stripAccents(event.content.toLowerCase))
 
   override def apply(pattern: Regex, event: MessageEvent)(using Logger[IO]): IO[Boolean] =
     val embed = Embed(
