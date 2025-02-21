20e0915ae466cf6347b68f5a7c57c8c10008063b
Aifosi <sissygasmquest@gmail.com>
2025-02-21 23:35:01 +0000

    Handle word alternations a bit better

--- src/main/scala/cuteguard/Fitness.scala
+++ src/main/scala/cuteguard/Fitness.scala
@@ -39,13 +39,14 @@ class Fitness(quadgrams: Deferred[IO, Map[String, Double]]):
 object Fitness:
   extension (string: String)
     def sanitise: String =
-      stripAccents(string)                          // Remove diacritics
-        .toLowerCase                                // to lowercase
-        .replaceAll("https?://[^ ]+\\.[^ ]+", "")   // remove links
-        .replaceAll("<a?:\\w+:\\d+>", "")           // remove emoji
-        .replaceAll("<sound:\\d+:\\d+>", "")        // remove sounds
-        .replaceAll("`(:?``)?[^`]+`(:?``)?", "")    // remove code blocks
-        .replaceAll("(\\w+)[^\\w ](\\w+)", "$1 $2") // remove word alternations
-        .replaceAll("[^a-z \n]", "")                // Remove all symbols
-        .replaceAll("(\\w)\\1{2,}", "$1")           // remove triples or longer
+      stripAccents(string)                            // Remove diacritics
+        .toLowerCase                                  // to lowercase
+        .replaceAll("https?://[^ ]+\\.[^ ]+", "")     // remove links
+        .replaceAll("<a?:\\w+:\\d+>", "")             // remove emoji
+        .replaceAll("<sound:\\d+:\\d+>", "")          // remove sounds
+        .replaceAll("`(:?``)?[^`]+`(:?``)?", "")      // remove code blocks
+        .replaceAll("([^\\w ])+", "$1")               // remove repeated non word and non spaces
+        .replaceAll("(\\w+?)[^\\w ](\\w+?)", "$1 $2") // remove word alternations
+        .replaceAll("[^a-z \n]", "")                  // Remove all symbols
+        .replaceAll("(\\w)\\1{2,}", "$1")             // remove triples or longer
         .replaceAll("(\\w{2,})\\1+", "$1") // remove word repetitions
