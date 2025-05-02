74842fad67e5d2e15cdd33eb73e8975e9baa11f2
Aifosi <sissygasmquest@gmail.com>
2025-05-02 14:26:47 +0100

    Use the right preference for subsmash

--- src/main/scala/cuteguard/commands/Subsmash.scala
+++ src/main/scala/cuteguard/commands/Subsmash.scala
@@ -49,7 +49,7 @@ case class Subsmash(
     List(
       Subsmash.best(fitness, config)(event.content, members).map(_.exists(_(2) > config.threshold)),
       cooldown.addEventAndCheckReady(event.author, Action.NotCute),
-      preferences.find(event.author, label = Some("subsmash")).fold(false)(_.notCuteOptOut),
+      preferences.find(event.author, label = Some("subsmash")).fold(false)(_.subsmashOptOut),
     ).foldLeft(IO.pure(true)) { case (acc, io) =>
       acc.flatMap {
         case false => IO.pure(false)
